341f82990c8f87066ae9aa38eca2cccf96a27bf6
Nayiem Willems <37852059+NayiemWillems@users.noreply.github.com>
2019-02-18 02:41:06 +0100

    Update network-state.ts

--- packages/core-p2p/src/network-state.ts
+++ packages/core-p2p/src/network-state.ts
@@ -99,7 +99,7 @@ export class NetworkState {
         const lastBlock = app.resolvePlugin("blockchain").getLastBlock();
 
         const peers = monitor.getPeers();
-        const minimumNetworkReach = localConfig.get("minimumNetworkReach", 20);
+        const minimumNetworkReach = localConfig.get("minimumNetworkReach", 1);
 
         if (monitor.__isColdStartActive()) {
             return new NetworkState(NetworkStateStatus.ColdStart, lastBlock);
