bc46854a55d685cf5ffa57d16b414c028c575f75
Nayiem Willems <37852059+NayiemWillems@users.noreply.github.com>
2019-03-10 18:08:10 +0100

    Update index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -17,7 +17,7 @@ function registerCommand(name: string, description: string): any {
         .description(description)
         .option("-d, --data <data>", "data directory")
         .option("-c, --config <config>", "core config")
-        .option("-t, --token <token>", "token name", "persona")
+        .option("-t, --token <token>", "token name", "phantom")
         .option("-n, --network <network>", "token network")
         .option("-r, --remote <remote>", "remote peer for config")
         .option("--network-start", "force genesis network start", false)
